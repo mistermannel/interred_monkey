3e015e1b316dd4b64a206755085e991bfd605e12
Anna Damm <anna.damm@netdoktor.de>
2017-09-13 11:14:23 +0200

    added some more usability to form inputs and links

--- nd_article_info.user.js
+++ nd_article_info.user.js
@@ -2,105 +2,112 @@
 // @name ND Article ID
 // @namespace ND
 // @author Netdoktor.de GmbH
-// @include http://www.netdoktor.de/*
-// @include https://www.netdoktor.de/*
-// @include http://stage.netdoktor.de/*
-// @include https://stage.netdoktor.de/*
-// @include /^.*cms\.nd\-intern:.*/
+// @include /^https?://www\.netdoktor\.de/
+// @include /^https?://stage\.netdoktor\.de/
+// @include /^https?://cms\.nd\-intern:\d+/
+// @include /^https?://(.*\.)?netdoktor\.dev/
 // @downloadURL https://raw.githubusercontent.com/NetDoktorDE/interred_monkey/master/nd_article_info.user.js
 // @updateURL https://raw.githubusercontent.com/NetDoktorDE/interred_monkey/master/nd_article_info.user.js
-// @version 2.10
+// @version 2.11
 // @grant none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    
+    /*global dataLayer */
     var ndirmFunctions = window.ndirmFunctions = {};
-	var ndirmVersion = "2.10"
+    var ndirmVersion = "2.11";
 
     /*
      * article navigation
      */
     ndirmFunctions.gotoArticle = function () {
-        var articleId = document.getElementById("ndirm-goto-id").value;
-        window.location.href="http://www.netdoktor.de/content/" + articleId;
+        var articleId        = document.getElementById("ndirm-goto-id").value;
+        window.location.href = "http://www.netdoktor.de/content/" + articleId;
     };
 
     /*
      * page type
      */
     var pageType = 'n/a';
-    var pageId = 'n/a';
-    if(dataLayer[0].page.pageType == 'interred') {
+    var pageId   = 'n/a';
+    if (dataLayer[0].page.pageType === 'interred') {
         pageType = 'IR article';
         try {
             pageId = dataLayer[0].page.articleID;
-        } catch(error) {}
+        } catch (error) {
+        }
     } else {
         pageType = 'CMS page';
         try {
             pageId = dataLayer[0].page.cmsPageId;
-        } catch(error) {}
+        } catch (error) {
+        }
     }
 
     /*
      * meta info
      */
     var metaInfo = '';
-    if(dataLayer[0].page.pageType == 'interred') {
+    if (dataLayer[0].page.pageType === 'interred') {
         var csId = 'n/a';
         try {
             csId = dataLayer[0].page.content.csId === undefined ? 'not set' : dataLayer[0].page.content.csId;
-        } catch(error) {}
+        } catch (error) {
+        }
         var articleTopic = 'n/a';
         try {
             var topics = [];
             for (var key in dataLayer[0].page.topic.main) {
                 topics.push(dataLayer[0].page.topic.main[key]);
             }
-            if(topics.length > 0) {
-                articleTopic = '<span class="ndirm-selectall">'+topics.join('</span>&nbsp;/ <span class="ndirm-selectall">')+'</span>';
+            if (topics.length > 0) {
+                articleTopic = '<span class="ndirm-selectall">' + topics.join('</span>&nbsp;/ <span class="ndirm-selectall">') + '</span>';
             }
-        } catch(error) {}
+        } catch (error) {
+        }
         var interredType = 'no type';
         try {
             interredType = dataLayer[0].page.articleType.interredType;
-        } catch(error) {}
+        } catch (error) {
+        }
         var articleType = 'no type';
         try {
             articleType = dataLayer[0].page.articleType.type;
-            if(dataLayer[0].page.articleType.subType !== undefined) {
-                articleType = articleType + '&nbsp;| '+dataLayer[0].page.articleType.subType;
+            if (dataLayer[0].page.articleType.subType !== undefined) {
+                articleType = articleType + '&nbsp;| ' + dataLayer[0].page.articleType.subType;
             }
-        } catch(error) {}
+        } catch (error) {
+        }
         // infos
         metaInfo =
             '  <div>' +
             '    <md-divider></md-divider><div class="ndirm-acc-head ndirm-infohead ndirm-link ndirm-icons-down">Meta Info</div>' +
             '    <div class="ndirm-acc-panel-open ndirm-infocontent">' +
-            '    <strong>cs-id: </strong>'+csId+'<br />' +
-            '    <strong>main topic: </strong>'+articleTopic+'<br />' +
+            '    <strong>cs-id: </strong>' + csId + '<br />' +
+            '    <strong>main topic: </strong>' + articleTopic + '<br />' +
             '    <strong>source: </strong>interred<br />' +
-            '    <strong>interred-type: </strong>'+interredType+'<br />' +
-            '    <strong>article-type: </strong>'+articleType+'<br />' +
+            '    <strong>interred-type: </strong>' + interredType + '<br />' +
+            '    <strong>article-type: </strong>' + articleType + '<br />' +
             '    </div>' +
             '  </div>';
     } else {
         var pageCsId = 'n/a';
         try {
             pageCsId = dataLayer[0].page.content.csId;
-        } catch(error) {}
+        } catch (error) {
+        }
         var pageDate = 'n/a';
         try {
             pageDate = dataLayer[0].page.publishDate;
-        } catch(error) {}
+        } catch (error) {
+        }
         metaInfo =
             '  <div>' +
             '    <md-divider></md-divider><div class="ndirm-acc-head ndirm-infohead ndirm-link ndirm-icons-down">Meta Info</div>' +
             '    <div class="ndirm-acc-panel-open ndirm-infocontent">' +
-            '    <strong>cs-id: </strong>'+pageCsId+'<br />' +
-            '    <strong>created: </strong>'+pageDate+'<br />' +
+            '    <strong>cs-id: </strong>' + pageCsId + '<br />' +
+            '    <strong>created: </strong>' + pageDate + '<br />' +
             '    </div>' +
             '  </div>';
     }
@@ -110,28 +117,32 @@
      */
     // method to set sourcepoint environment to stage or public
     ndirmFunctions.setSourcepointEnv = function (envName) {
-        if((envName == 'stage') || (envName == 'public')) {
-            document.getElementById("sourcepointEnvIFrame").src='http://ressource1.netdoktor.de/mms/qa_set_env?env=' + envName;
+        if ((envName === 'stage') || (envName === 'public')) {
+            document.getElementById("sourcepointEnvIFrame").src = 'http://ressource1.netdoktor.de/mms/qa_set_env?env=' + envName;
         }
     };
     // controls to set sourcepoint env
-    var codeSourcePoint =
-        '  <div>' +
-        '    <md-divider></md-divider><div class="ndirm-acc-head ndirm-infohead ndirm-link ndirm-icons-down">SourcePoint</div>' +
-        '    <div class="ndirm-acc-panel-open ndirm-infocontent">' +
-        '      <iframe id="sourcepointEnvIFrame" height="0" width="0" border="0" style="border:0px;"></iframe>' +
-        '      <strong>sourcepoint: </strong><a onClick="javascript:window.ndirmFunctions.setSourcepointEnv(\'stage\');" href="#" style="font-size:14px;">stage</a> | <a onClick="javascript:window.ndirmFunctions.setSourcepointEnv(\'public\');" href="#" style="font-size:14px;">public</a>' +
-        '    </div>' +
-        '  </div>';
+    var codeSourcePoint              =
+            '  <div>' +
+            '    <md-divider></md-divider><div class="ndirm-acc-head ndirm-infohead ndirm-link ndirm-icons-down">SourcePoint</div>' +
+            '    <div class="ndirm-acc-panel-open ndirm-infocontent">' +
+            '      <iframe id="sourcepointEnvIFrame" height="0" width="0" style="border:0;"></iframe>' +
+            '      <strong>sourcepoint: </strong>' +
+            '<a onClick="window.ndirmFunctions.setSourcepointEnv(\'stage\');" href="javascript:void(0);" style="font-size:14px;">stage</a> | ' +
+            '<a onClick="window.ndirmFunctions.setSourcepointEnv(\'public\');" href="javascript:void(0);" style="font-size:14px;">public</a>' +
+            '    </div>' +
+            '  </div>';
 
     /*
      * main object
      */
-    var editorialInfoDiv = document.createElement('div');
+    var editorialInfoDiv       = document.createElement('div');
     editorialInfoDiv.className = "article-icd";
-    editorialInfoDiv.innerHTML = '<div class="ndirm-infobox"><div class="ndirm-acc-head ndirm-infotitle ndirm-icons-down"><span class="ndirm-link">'+pageType+'</span> (id:<span class="ndirm-selectall">'+pageId+'</span>)</div>' +
-        '<div class="ndirm-acc-panel">' +
-        '<input id="ndirm-goto-id" class="ndirm-input" placeholder="Enter article id" type="text" style="width:100px;" onClick=""></input>&nbsp;&nbsp;<a href="#" onClick="javascript:window.ndirmFunctions.gotoArticle()" class="ndirm-link">Go to article</a>' +
+    editorialInfoDiv.innerHTML = '<div class="ndirm-infobox"><div class="ndirm-acc-head ndirm-infotitle ndirm-icons-down"><span class="ndirm-link">' + pageType +
+        '</span> (id:<span class="ndirm-selectall">' + pageId + '</span>)</div>' +
+        '<div class="ndirm-acc-panel"><form onsubmit="window.ndirmFunctions.gotoArticle(); return false;">' +
+        '<input id="ndirm-goto-id" class="ndirm-input" placeholder="Enter article id" type="number" style="width:100px;" required/>&nbsp;&nbsp;' +
+        '<a href="javascript:void(0);" onClick="form.submit();" class="ndirm-link">Go to article</a></form>' +
         metaInfo +
         codeSourcePoint +
         '<md-divider></md-divider>' +
@@ -140,15 +151,15 @@
         '<img class="ndirm-image" src="https://cdn.netdoktor.de/images/internal/ir-monkey.jpg" width="180px" />' +
         '<div class="ndirm-name">Interred Monkey</div>' +
         '</div>' +
-        '<div class="ndirm-footer">v '+ndirmVersion+'</div>' +
+        '<div class="ndirm-footer">v ' + ndirmVersion + '</div>' +
         '</div>';
 
     var pageBodyTags = document.getElementsByTagName('body');
-    if((pageBodyTags !== 'undefined') && (pageBodyTags[0] !== 'undefined')) {
+    if ((pageBodyTags !== 'undefined') && (pageBodyTags[0] !== 'undefined')) {
         var pageBody = pageBodyTags[0];
         pageBody.insertBefore(editorialInfoDiv, pageBody.firstChild);
         // style
-        var editorialInfoStyle = document.createElement('style');
+        var editorialInfoStyle       = document.createElement('style');
         editorialInfoStyle.innerHTML = '.ndirm-selectall { -webkit-user-select: all; -moz-user-select: all; -ms-user-select: all; user-select: all; }' +
             '.ndirm-infobox { z-index:99; margin:10px; padding:8px 16px 8px 16px; background-color: #ffffff; background-color: var(--white); box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5); position: fixed; top: 0;px left: 0; width: 300px; background: #fff; opacity: 0.9; font-family:Roboto, sans-serif; font-size:14px; line-height:23.8px; text-size-adjust:100%; -webkit-font-smoothing:antialiased; -webkit-tap-highlight-color:rgba(0, 0, 0, 0); }' +
             '.ndirm-infotitle { margin-top:8px; margin-left:0px; margin-bottom:8px; }' +
@@ -164,15 +175,19 @@
             '.ndirm-image-box {  }' +
             '.ndirm-image { width:180px; border-radius:50%; display: block; margin-left: auto; margin-right: auto; }' +
             '.ndirm-footer { font-size: 8px; }' +
-            '.ndirm-link { font-weight:500; color:#00bef7; font-size: 14px; }';
+            '.ndirm-link { font-weight:500; color:#00bef7; font-size: 14px; cursor: pointer; }' +
+            '.ndirm-acc-head { cursor: pointer; }' +
+            'input.ndirm-input[type=number]::-webkit-inner-spin-button, \n' +
+            'input.ndirm-input[type=number]::-webkit-outer-spin-button {\n -webkit-appearance: none; }' +
+            'input.ndirm-input[type=number] { -moz-appearance:textfield; }';
         pageBody.insertBefore(editorialInfoStyle, pageBody.firstChild);
     }
 
     var acc = document.getElementsByClassName("ndirm-acc-head");
     var i;
-    console.log(acc);
+
     for (i = 0; i < acc.length; i++) {
-        acc[i].onclick = function(){
+        acc[i].onclick = function () {
             /* Toggle between adding and removing the "active" class,
             to highlight the button that controls the panel */
             this.classList.toggle("ndirm-icons-up");
