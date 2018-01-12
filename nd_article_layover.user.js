7e56a4e3a7c970539f4f3f3b0c547d7032b0b20e
Anna Damm <Arany@users.noreply.github.com>
2018-01-12 11:54:07 +0100

    Fixed not defined variable errors, simplified matching regex

--- nd_article_layover.user.js
+++ nd_article_layover.user.js
@@ -10,15 +10,15 @@
 // @include /^https?://10.14.6.23:\d+/
 // @downloadURL https://raw.githubusercontent.com/NetDoktorDE/interred_monkey/master/nd_article_layover.user.js
 // @updateURL https://raw.githubusercontent.com/NetDoktorDE/interred_monkey/master/nd_article_layover.user.js
-// @version 1.2.1
+// @version 1.2.2
 // @grant none
 // ==/UserScript==
 
 (function () {
     'use strict';
-    var mySections = document.querySelectorAll("div.section");
-    for (i = 0; i < mySections.length; i++) {
-        var titleDiv = document.createElement('div');
+    let mySections = document.querySelectorAll("div.section");
+    for (let i = 0; i < mySections.length; i++) {
+        let titleDiv = document.createElement('div');
         titleDiv.innerHTML = getNarmalizedNameFromClass(mySections[i].className);
         titleDiv.style.position = "absolute";
         titleDiv.style.backgroundColor = "#999999";
@@ -26,17 +26,17 @@
         mySections[i].insertBefore(titleDiv, mySections[i].firstChild);
     }
     mySections = document.querySelectorAll("div.section > div > div.left");
-    for (i = 0; i < mySections.length; i++) {
+    for (let i = 0; i < mySections.length; i++) {
         mySections[i].style.border = "1px solid #dddddd99";
         mySections[i].style.backgroundColor = "#dddddd55";
     }
     mySections = document.getElementsByClassName("slot");
-    for (i = 0; i < mySections.length; i++) {
+    for (let i = 0; i < mySections.length; i++) {
         mySections[i].style.border = "1px solid #00bef799";
         mySections[i].style.minHeight="32px";
         mySections[i].style.backgroundColor = "#00bef755";
 
-        var titleDiv = document.createElement('div');
+        let titleDiv = document.createElement('div');
         titleDiv.innerHTML = getNarmalizedNameFromClass(mySections[i].className);
         titleDiv.style.position = "absolute";
         titleDiv.style.backgroundColor = "#999999";
@@ -44,9 +44,9 @@
         mySections[i].insertBefore(titleDiv, mySections[i].firstChild);
     }
     mySections = document.getElementsByTagName("iq-ad");
-    for (i = 0; i < mySections.length; i++) {
-        var device = mySections[i].getAttribute('slot');
-        var adType = '';
+    for (let i = 0; i < mySections.length; i++) {
+        let device = mySections[i].getAttribute('slot');
+        let adType = '';
         if(
             ((window.innerWidth >= 768) && (device == 'desktop')) ||
             ((window.innerWidth < 768) && (device == 'mobile'))
@@ -56,7 +56,7 @@
             mySections[i].style.minHeight="32px";
             mySections[i].style.backgroundColor = "#ee7f0055";
 
-            var titleDiv = document.createElement('div');
+            let titleDiv = document.createElement('div');
             titleDiv.innerHTML = adType;
             titleDiv.style.position = "absolute";
             titleDiv.style.zIndex = "99999";
@@ -67,11 +67,7 @@
     }
 
     function getNarmalizedNameFromClass(className) {
-        var matching = className.match(/slot-section-(margin-[0-9])/);
-        if(matching !== null) {
-            return matching[1];
-        }
-        matching = className.match(/slot-section-(container-[0-9])/);
+        let matching = className.match(/slot-section-((margin|container)-[0-9])/);
         if(matching !== null) {
             return matching[1];
         }
